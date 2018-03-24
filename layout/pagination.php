267dfe20d1fcfc2596793975e0806d257eb25b93
rulyaryu <rulyaryu@gmail.com>
2018-03-24 23:18:17 +0200

    modified model4 and pgination

--- layout/pagination.php
+++ layout/pagination.php
@@ -1,22 +1,20 @@
 <?php 
 	require './model4.php';
+	require './link.php';
  ?>
 
 <div class="coontainer d-flex justify-content-center mt-5 mb-5">
 	<nav aria-label="Books Page Navigation">
 	  <ul class="pagination">
 	    <li class="page-item <?= $page == 1 ? 'disabled' : '' ?>">
-	      <a class="page-link" href="index.php?page=1<?=isset($cat) ? '&cat=' . urlencode($_GET['cat'])	: ''?>" aria-label="Previous">
+	      <a class="page-link" href="index.php?page=1<?= doOrDoNotLink('cat') . doOrDoNotLink('q')?>" aria-label="Previous">
 	        <span aria-hidden="true">&laquo;</span>
 	        <span class="sr-only">Previous</span>
 	      </a>
 	    </li>
 
 		<li class="page-item <?= $page == 1 ? 'disabled' : '' ?>">
-					<a class="page-link" href="index.php?page=<?= $page-1 ?><?=isset($cat) 
-																																					? '&cat=' . urlencode($_GET['cat'])
-																																					: ''
-					?>">Previous</a>
+					<a class="page-link" href="index.php?page=<?= $page-1 ?><?=doOrDoNotLink('cat')	. doOrDoNotLink('q')?>">Previous</a>
     	</li>
 
 	    <li class="page-item active">
@@ -25,18 +23,11 @@
 
 
 	    <li class="page-item <?= $page == $limitPage ? 'disabled' : '' ?>">
-					<a class="page-link" href="index.php?page=<?= $page+1?><?=isset($cat) 
-																																					? '&cat=' . urlencode($_GET['cat'])
-																																					: ''
-					
-					?>">Next</a>
+					<a class="page-link" href="index.php?page=<?= $page+1?><?=doOrDoNotLink('cat') . doOrDoNotLink('q')?>">Next</a>
     	</li>
 
 	    <li class="page-item <?= $page == $limitPage ? 'disabled' : '' ?>">
-				<a class="page-link" href="index.php?page=<?=$limitPage?><?=isset($cat) 
-																																					? '&cat=' . urlencode($_GET['cat'])
-																																					: ''	
-				?>" aria-label="Next">
+				<a class="page-link" href="index.php?page=<?=$limitPage?><?=doOrDoNotLink('cat') . doOrDoNotLink('q')?>" aria-label="Next">
 	        <span aria-hidden="true">&raquo;</span>
 	        <span class="sr-only">Next</span>
 	      </a>
